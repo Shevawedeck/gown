8a29006351e0264270bd8085a031b7c08de2fbb6
shevawedeck <shevawedeck@gmail.com>
2022-12-29 18:37:05 -0500

    gown scenario

---
+++ 0 scenario spec.sql
@@ -0,0 +1,24 @@
+/* Hi, I run a kids' gown gemach for my neighborhood.  We lend out gowns free of charge.  Everyone who takes a gown must give in a check of $70 per gown.  If the gown is returned on time, in good condition
+the customers get their check back.  If the gown is returned more than a week late and/or with any damage the check is deposited.  Customers can have the gown for a total of two weeks so they can do 
+necesary alterations and dry cleaning. 
+I need to have a better way to keep track of dates the gown is used and whos checks I need to deposit.  
+I need to see gown brand, style, color, size, who borrowed it and when they borrowed and returned it, the gown's condition before and after it was borrowed and how many times it was borrowed.
+I also want a column that says if the check has to be deposited or not.
+
+Do you have multiples of gowns?
+Yes, we have multiples of gowns
+
+When did you start lending gowns?
+April 16, 1997 was the first time I lent a gown.
+
+How would you rate conditions?
+Excellent, Good, Fine, Bad
+
+Sample data:
+Sharon Weiss - Pullini puffy lavendar gown size 4, borrowed on 9/17/21 returned 9/25/21 - borrowed and returned in excellent condition
+Sharon Weiss - Pullini puffy lavendar gown size 6, borrowed on 9/17/21 returned 9/25/21 - borrowed in good condition, returned in fine condition
+Chayala Halpert - Shar tulle pale pink gown size 8, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in good condition
+Chayala Halpert - Shar tulle pale pink gown size 6, borrowed on 8/10/21 returned 8/27/21 - borrowed in good condition and returned in fine condition
+Chayala Halpert - Pullini puffy pale pink gown size 4, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in fine condition
+Chayala Halpert - Pullini puffy pale pink gown size 2, borrowed on 8/10/21 returned 8/27/21 - borrowed in fine condition and returned in bad condition
+*/
