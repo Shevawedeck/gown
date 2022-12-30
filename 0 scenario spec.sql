1bc5c11c5702a0249453a665d30d3433818f6fe6
shevawedeck <shevawedeck@gmail.com>
2022-12-29 19:04:25 -0500

    gown corrected

--- 0 scenario spec.sql
+++ 0 scenario spec.sql
@@ -4,9 +4,14 @@ necesary alterations and dry cleaning.
 I need to have a better way to keep track of dates the gown is used and whos checks I need to deposit.  
 I need to see gown brand, style, color, size, who borrowed it and when they borrowed and returned it, the gown's condition before and after it was borrowed and how many times it was borrowed.
 I also want a column that says if the check has to be deposited or not.
+Also I want a column of which # gown it is(we have multiples of gown so we assign each gown a number)
 
+Questions:
 Do you have multiples of gowns?
 Yes, we have multiples of gowns
+
+What is the highest number of multiples of gowns?
+We have never had more than fifteen of the same exact gown.
 
 When did you start lending gowns?
 April 16, 1997 was the first time I lent a gown.
@@ -14,11 +19,20 @@ April 16, 1997 was the first time I lent a gown.
 How would you rate conditions?
 Excellent, Good, Fine, Bad
 
+How do the numbers work?
+If we have five of the same gowns in the same size and color each one has a number from 1-5
+If we only have one gown in that size and color it gets the number one
+
+Reports:
+1. I need to see all size 6 gowns ever borrowed
+2. I need a list of which checks must be deposited
+3. I want to see how many times each gown was borrowed
+
 Sample data:
-Sharon Weiss - Pullini puffy lavendar gown size 4, borrowed on 9/17/21 returned 9/25/21 - borrowed and returned in excellent condition
-Sharon Weiss - Pullini puffy lavendar gown size 6, borrowed on 9/17/21 returned 9/25/21 - borrowed in good condition, returned in fine condition
-Chayala Halpert - Shar tulle pale pink gown size 8, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in good condition
-Chayala Halpert - Shar tulle pale pink gown size 6, borrowed on 8/10/21 returned 8/27/21 - borrowed in good condition and returned in fine condition
-Chayala Halpert - Pullini puffy pale pink gown size 4, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in fine condition
-Chayala Halpert - Pullini puffy pale pink gown size 2, borrowed on 8/10/21 returned 8/27/21 - borrowed in fine condition and returned in bad condition
+Sharon Weiss - Pullini puffy lavendar gown size 6, number 2, borrowed on 9/17/21 returned 9/25/21 - borrowed and returned in excellent condition
+Sharon Weiss - Pullini puffy lavendar gown size 6, number 1, borrowed on 9/17/21 returned 9/25/21 - borrowed in good condition, returned in fine condition
+Chayala Halpert - Shar tulle pale pink gown size 8, number 2, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in good condition
+Chayala Halpert - Shar tulle pale pink gown size 6, number 1, borrowed on 8/10/21 returned 8/27/21 - borrowed in good condition and returned in fine condition
+Chayala Halpert - Pullini puffy pale pink gown size 4, number 3, borrowed on 8/10/21 returned 8/27/21 - borrowed and returned in fine condition
+Chayala Halpert - Pullini puffy pale pink gown size 2, number 2, borrowed on 8/10/21 returned 8/27/21 - borrowed in fine condition and returned in bad condition
 */
